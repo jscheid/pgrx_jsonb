777c01c72062b74127e562f179d2810c7f7a7450
Julian Scheid <julians37@gmail.com>
2023-11-14 02:48:58 +0100

    Pull, not push

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use core::ffi::CStr;
+use ouroboros::self_referencing;
 use pgrx::pg_sys::{
     jbvType_jbvArray as JBV_ARRAY, jbvType_jbvBinary as JBV_BINARY, jbvType_jbvBool as JBV_BOOL,
     jbvType_jbvDatetime as JBV_DATETIME, jbvType_jbvNull as JBV_NULL,
@@ -10,8 +11,6 @@ use pgrx::pg_sys::{
     JsonbIteratorToken_WJB_END_OBJECT as WJB_END_OBJECT, JsonbIteratorToken_WJB_KEY as WJB_KEY,
     JsonbIteratorToken_WJB_VALUE as WJB_VALUE,
     JsonbValue__bindgen_ty_1__bindgen_ty_1 as JsonbRawString,
-    JsonbValue__bindgen_ty_1__bindgen_ty_2 as JsonbRawArray,
-    JsonbValue__bindgen_ty_1__bindgen_ty_3 as JsonbRawObject,
 };
 use pgrx::prelude::*;
 use pgrx::{direct_function_call, pg_sys};
@@ -29,16 +28,16 @@ pub enum JsonbTraversal {
 }
 
 /// An opaque reference to a string inside a JSONB document.
-pub struct JsonbString<'a>(&'a JsonbRawString);
+pub struct JsonbString<'a> {
+    inner: JsonbRawString,
+    _covariant: std::marker::PhantomData<&'a ()>,
+}
 
 /// An opaque reference to a numeric inside a JSONB document.
-pub struct JsonbNumeric<'a>(&'a pg_sys::Numeric);
-
-/// An opaque reference to an array inside a JSONB document.
-pub struct JsonbArray<'a>(&'a JsonbRawArray);
-
-/// An opaque reference to an object inside a JSONB document.
-pub struct JsonbObject<'a>(&'a JsonbRawObject);
+pub struct JsonbNumeric<'a> {
+    inner: pg_sys::Numeric,
+    _covariant: std::marker::PhantomData<&'a ()>,
+}
 
 /// A value inside a JSONB document.
 pub enum JsonbScalar<'a> {
@@ -48,146 +47,15 @@ pub enum JsonbScalar<'a> {
     Bool(bool),
 }
 
-pub trait JsonbVisitor<T, E> {
-    fn begin_array(&mut self, num_elems: usize) -> Result<JsonbTraversal, E>;
-    fn end_array(&mut self) -> Result<JsonbTraversal, E>;
-    fn begin_object(&mut self, num_pairs: usize) -> Result<JsonbTraversal, E>;
-    fn end_object(&mut self) -> Result<JsonbTraversal, E>;
-    fn key(&mut self, val: JsonbScalar) -> Result<JsonbTraversal, E>;
-    fn value(&mut self, val: JsonbScalar) -> Result<JsonbTraversal, E>;
-    fn elem(&mut self, val: JsonbScalar) -> Result<JsonbTraversal, E>;
-    fn done(&mut self) -> Result<T, E>;
-}
-
-#[derive(Debug)]
-enum SerdeValueBuilderState {
-    Value(serde_json::Value),
-    Array(Vec<serde_json::Value>),
-    Object(serde_json::Map<String, serde_json::Value>, Option<String>),
-}
-
-#[derive(Default)]
-struct SerdeValueBuilder {
-    state_stack: Vec<SerdeValueBuilderState>,
-}
-
-impl SerdeValueBuilder {
-    fn push_value(&mut self, out: serde_json::Value) {
-        match self.state_stack.last_mut() {
-            Some(SerdeValueBuilderState::Array(vec)) => {
-                vec.push(out);
-            }
-            Some(SerdeValueBuilderState::Object(map, key)) => {
-                let key = key.take().expect("Unexpected state: missing key");
-                map.insert(key, out);
-            }
-            None => {
-                self.state_stack.push(SerdeValueBuilderState::Value(out));
-            }
-            other => panic!("Unexpected state {other:?} while pushing value"),
-        }
-    }
-}
-
-impl JsonbVisitor<serde_json::Value, ()> for SerdeValueBuilder {
-    fn begin_array(&mut self, num_elems: usize) -> Result<JsonbTraversal, ()> {
-        self.state_stack
-            .push(SerdeValueBuilderState::Array(Vec::with_capacity(num_elems)));
-        Ok(JsonbTraversal::StepInto)
-    }
-
-    fn elem(&mut self, val: JsonbScalar) -> Result<JsonbTraversal, ()> {
-        match self.state_stack.last_mut() {
-            Some(SerdeValueBuilderState::Array(ref mut vec)) => {
-                vec.push(val.to_serde_json_value()?);
-            }
-            other => {
-                panic!("Unexpected state {other:?} for elem");
-            }
-        }
-        Ok(JsonbTraversal::StepInto)
-    }
-
-    fn end_array(&mut self) -> Result<JsonbTraversal, ()> {
-        match self.state_stack.pop() {
-            Some(SerdeValueBuilderState::Array(val)) => {
-                self.push_value(serde_json::Value::Array(val));
-            }
-            other => {
-                panic!("Unexpected state {other:?} for end_array");
-            }
-        }
-        Ok(JsonbTraversal::StepInto)
-    }
-
-    fn begin_object(&mut self, num_pairs: usize) -> Result<JsonbTraversal, ()> {
-        self.state_stack.push(SerdeValueBuilderState::Object(
-            serde_json::Map::with_capacity(num_pairs),
-            None,
-        ));
-        Ok(JsonbTraversal::StepInto)
-    }
-
-    fn key(&mut self, val: JsonbScalar) -> Result<JsonbTraversal, ()> {
-        let key = if let JsonbScalar::String(str) = val {
-            str.as_ref().to_os_string().into_string().or(Err(()))?
-        } else {
-            panic!("Non-String key");
-        };
-
-        if let Some(SerdeValueBuilderState::Object(_, ref mut maybe_key)) =
-            self.state_stack.last_mut()
-        {
-            assert!(maybe_key.replace(key).is_none());
-        } else {
-            panic!("Unexpected state");
-        }
-        Ok(JsonbTraversal::StepInto)
-    }
-
-    fn value(&mut self, val: JsonbScalar) -> Result<JsonbTraversal, ()> {
-        if let Some(SerdeValueBuilderState::Object(ref mut map, ref mut maybe_key)) =
-            self.state_stack.last_mut()
-        {
-            let key = maybe_key.take().expect("Missing key");
-            map.insert(key, val.to_serde_json_value()?);
-        } else {
-            panic!("Unexpected state");
-        }
-        Ok(JsonbTraversal::StepInto)
-    }
-
-    fn end_object(&mut self) -> Result<JsonbTraversal, ()> {
-        match self.state_stack.pop() {
-            Some(SerdeValueBuilderState::Object(map, None)) => {
-                self.push_value(serde_json::Value::Object(map));
-            }
-            other => {
-                panic!("Unexpected state {other:?} for end_object");
-            }
-        }
-        Ok(JsonbTraversal::StepInto)
-    }
-
-    fn done(&mut self) -> Result<serde_json::Value, ()> {
-        match self.state_stack.pop() {
-            Some(SerdeValueBuilderState::Value(val)) => {
-                assert!(self.state_stack.is_empty());
-                Ok(val)
-            }
-            other => {
-                panic!("Unexpected state {other:?} for done");
-            }
-        }
-    }
-}
-
 impl<'a> AsRef<OsStr> for JsonbString<'a> {
     fn as_ref(&self) -> &OsStr {
         let slice = unsafe {
             std::slice::from_raw_parts(
-                self.0.val as *mut u8,
-                self.0.len.try_into().expect("i32 should fit into usize"),
+                self.inner.val as *mut u8,
+                self.inner
+                    .len
+                    .try_into()
+                    .expect("i32 should fit into usize"),
             )
         };
         OsStr::from_bytes(slice)
@@ -199,7 +67,7 @@ impl<'a> AsRef<CStr> for JsonbNumeric<'a> {
         unsafe {
             direct_function_call::<&CStr>(
                 pg_sys::numeric_out,
-                &[Some(pg_sys::Datum::from(*self.0))],
+                &[Some(pg_sys::Datum::from(self.inner))],
             )
         }
         .expect("should return a &CStr")
@@ -232,32 +100,18 @@ impl<'a> fmt::Display for JsonbScalar<'a> {
     }
 }
 
-impl<'a> JsonbArray<'a> {
-    pub fn len(&self) -> usize {
-        self.0.nElems.try_into().expect("i32 should fit into usize")
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.0.nElems == 0
-    }
-}
-
-impl<'a> JsonbObject<'a> {
-    pub fn len(&self) -> usize {
-        self.0.nPairs.try_into().expect("i32 should fit into usize")
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.0.nPairs == 0
-    }
-}
-
 impl<'a> JsonbScalar<'a> {
-    fn from_pg_sys(val: &'a pg_sys::JsonbValue) -> Self {
+    fn from_pg_sys(val: pg_sys::JsonbValue) -> Self {
         match val.type_ {
             JBV_NULL => Self::Null,
-            JBV_STRING => Self::String(JsonbString(unsafe { &val.val.string })),
-            JBV_NUMERIC => Self::Number(JsonbNumeric(unsafe { &val.val.numeric })),
+            JBV_STRING => Self::String(JsonbString {
+                inner: unsafe { val.val.string },
+                _covariant: std::marker::PhantomData,
+            }),
+            JBV_NUMERIC => Self::Number(JsonbNumeric {
+                inner: unsafe { val.val.numeric },
+                _covariant: std::marker::PhantomData,
+            }),
             JBV_BOOL => Self::Bool(unsafe { val.val.boolean }),
             JBV_DATETIME => todo!("datetime support"),
             _ => panic!("Unknown JsonValue type"),
@@ -265,111 +119,168 @@ impl<'a> JsonbScalar<'a> {
     }
 }
 
-/// Iterate over the `jsonb` document, feeding results into the given
-/// `visitor`.
-///
-/// # Safety
-///
-/// Currently marked unsafe because it accepts a raw pointer and
-/// operates on it. Caller is currently responsible for passing
-/// in a correct pointer. This should be fixed by changing them
-/// argument type to some Datum.
-pub unsafe fn iterate_jsonb<V, E, S: JsonbVisitor<V, E>>(
-    jsonb: *mut pg_sys::Jsonb,
-    mut visitor: S,
-) -> Result<V, E> {
-    let mut it = pg_sys::JsonbIteratorInit(&mut (*jsonb).root);
-    let result = {
-        let mut val = pg_sys::JsonbValue::default();
-        let mut skip = JsonbTraversal::StepInto;
-
-        loop {
-            let token = unsafe {
-                pg_sys::JsonbIteratorNext(
-                    &mut it,
-                    &mut val,
-                    match skip {
-                        JsonbTraversal::StepInto => false,
-                        JsonbTraversal::SkipOver => true,
-                    },
-                )
-            };
-            skip = match token {
-                WJB_DONE => {
-                    break visitor.done();
-                }
-                WJB_KEY => visitor.key(JsonbScalar::from_pg_sys(&val))?,
-                token if matches!(token, WJB_VALUE | WJB_ELEM) => {
-                    if val.type_ == JBV_BINARY {
-                        todo!("binary handling");
-                        // Not yet sure what to do here:
-                        // - it doesn't seem like we can access the array/object in bulk
-                        // - we can report skipped_array/skipped_object with element/pair count to the visitor?
-                    } else {
-                        let value = JsonbScalar::from_pg_sys(&val);
-                        if token == WJB_VALUE {
-                            visitor.value(value)?
-                        } else {
-                            visitor.elem(value)?
-                        }
-                    }
-                }
-                WJB_BEGIN_ARRAY => {
-                    assert!(val.type_ == JBV_ARRAY);
-                    visitor.begin_array(
-                        val.val
-                            .array
-                            .nElems
-                            .try_into()
-                            .expect("i32 should fit into usize"),
-                    )?
-                }
-                WJB_END_ARRAY => visitor.end_array()?,
-                WJB_BEGIN_OBJECT => {
-                    assert!(val.type_ == JBV_OBJECT);
-                    visitor.begin_object(
-                        val.val
-                            .object
-                            .nPairs
-                            .try_into()
-                            .expect("i32 should fit into usize"),
-                    )?
-                }
-                WJB_END_OBJECT => visitor.end_object()?,
-                _ => panic!("Invalid iterator state"),
-            };
-        }
-    };
-
-    result
+struct JsonbBlob<'a> {
+    ptr: *mut pg_sys::Jsonb,
+    owned: bool,
+    _covariant: std::marker::PhantomData<&'a ()>,
 }
 
-#[pg_extern(sql = r#"
-    CREATE FUNCTION "jsonb_test"(jsonb) RETURNS boolean
-    STRICT
-    LANGUAGE c /* Rust */
-    AS '@MODULE_PATHNAME@', '@FUNCTION_NAME@';
-"#)]
-fn jsonb_test(datum: pg_sys::Datum) -> bool {
-    let varlena = datum.cast_mut_ptr();
-    let detoasted = unsafe { pg_sys::pg_detoast_datum_packed(varlena) };
-
-    let result = unsafe {
-        iterate_jsonb(
-            detoasted as *mut pg_sys::Jsonb,
-            SerdeValueBuilder::default(),
-        )
-        .unwrap()
-    };
-
-    if detoasted != varlena {
-        unsafe {
-            pg_sys::pfree(detoasted as pgrx::void_mut_ptr);
+impl<'a> Drop for JsonbBlob<'a> {
+    fn drop(&mut self) {
+        if self.owned {
+            unsafe {
+                pg_sys::pfree(self.ptr.cast());
+            }
         }
     }
+}
 
-    // Ensure nothing gets optimized away
-    matches!(result, serde_json::Value::Object(_))
+impl<'a> JsonbBlob<'a> {
+    fn new(varlena: *mut pg_sys::varlena) -> Self {
+        let ptr = unsafe { pg_sys::pg_detoast_datum_packed(varlena) };
+        Self {
+            ptr: ptr as *mut pg_sys::Jsonb,
+            owned: ptr != varlena,
+            _covariant: std::marker::PhantomData,
+        }
+    }
+}
+
+struct JsonbIteratorRef<'a> {
+    ptr: *mut pg_sys::JsonbIterator,
+    _covariant: std::marker::PhantomData<&'a ()>,
+}
+
+// impl<'a> Drop for JsonbIteratorRef<'a> {
+//     fn drop(&mut self) {
+//         unsafe {
+//             pg_sys::pfree(self.ptr.cast());
+//         }
+//     }
+// }
+
+#[self_referencing]
+struct JsonbIteratorInner<'a> {
+    detoasted: JsonbBlob<'a>,
+    #[borrows(mut detoasted)]
+    #[covariant]
+    it: JsonbIteratorRef<'this>,
+}
+
+struct JsonbIterator<'a>(JsonbIteratorInner<'a>);
+
+enum JsonbItem<'a> {
+    BeginArray(usize),
+    EndArray,
+    BeginObject(usize),
+    Value(JsonbScalar<'a>),
+    EndObject,
+}
+
+impl<'a> JsonbIterator<'a> {
+    fn new(detoasted: JsonbBlob<'a>) -> Self {
+        Self(
+            JsonbIteratorInnerBuilder {
+                detoasted,
+                it_builder: |detoasted: &'_ mut JsonbBlob<'_>| unsafe {
+                    JsonbIteratorRef {
+                        ptr: pg_sys::JsonbIteratorInit(&mut (*detoasted.ptr).root),
+                        _covariant: std::marker::PhantomData,
+                    }
+                },
+            }
+            .build(),
+        )
+    }
+
+    fn next<'b>(&'b mut self, traversal: JsonbTraversal) -> Option<JsonbItem<'b>> {
+        let mut val = pg_sys::JsonbValue::default();
+        let token = self.0.with_mut(|fields| unsafe {
+            pg_sys::JsonbIteratorNext(
+                &mut fields.it.ptr,
+                &mut val,
+                match traversal {
+                    JsonbTraversal::StepInto => false,
+                    JsonbTraversal::SkipOver => true,
+                },
+            )
+        });
+        match token {
+            WJB_DONE => None,
+            token if matches!(token, WJB_VALUE | WJB_ELEM | WJB_KEY) => {
+                if val.type_ == JBV_BINARY {
+                    todo!("binary handling");
+                    // Not yet sure what to do here:
+                    // - it doesn't seem like we can access the array/object in bulk
+                    // - we can report skipped_array/skipped_object with element/pair count to the visitor?
+                } else {
+                    Some(JsonbItem::Value(JsonbScalar::from_pg_sys(val)))
+                }
+            }
+            WJB_BEGIN_ARRAY => {
+                assert!(val.type_ == JBV_ARRAY);
+                Some(JsonbItem::BeginArray(unsafe {
+                    val.val
+                        .array
+                        .nElems
+                        .try_into()
+                        .expect("i32 should fit into usize")
+                }))
+            }
+            WJB_END_ARRAY => Some(JsonbItem::EndArray),
+            WJB_BEGIN_OBJECT => {
+                assert!(val.type_ == JBV_OBJECT);
+                Some(JsonbItem::BeginObject(unsafe {
+                    val.val
+                        .object
+                        .nPairs
+                        .try_into()
+                        .expect("i32 should fit into usize")
+                }))
+            }
+            WJB_END_OBJECT => Some(JsonbItem::EndObject),
+            _ => panic!("Invalid iterator state"),
+        }
+    }
+}
+
+fn read_value(it: &mut JsonbIterator) -> serde_json::Value {
+    match it.next(JsonbTraversal::StepInto) {
+        Some(JsonbItem::Value(val)) => val.to_serde_json_value().unwrap(),
+        Some(JsonbItem::BeginObject(num_elems)) => {
+            let mut map = serde_json::Map::with_capacity(num_elems);
+            loop {
+                let key = it.next(JsonbTraversal::StepInto);
+                match key {
+                    Some(JsonbItem::Value(JsonbScalar::String(key))) => {
+                        let key = key
+                            .as_ref()
+                            .to_os_string()
+                            .into_string()
+                            .or(Err(()))
+                            .unwrap();
+                        map.insert(key, read_value(it));
+                    }
+                    Some(JsonbItem::EndObject) => {
+                        return serde_json::Value::Object(map);
+                    }
+                    _ => panic!(),
+                }
+            }
+        }
+        Some(JsonbItem::BeginArray(num_elems)) => {
+            let mut vec = Vec::with_capacity(num_elems);
+            for _ in 0..num_elems {
+                vec.push(read_value(it))
+            }
+            assert!(matches!(
+                it.next(JsonbTraversal::StepInto),
+                Some(JsonbItem::EndArray)
+            ));
+            return serde_json::Value::Array(vec);
+        }
+        _ => panic!(),
+    }
 }
 
 #[pg_extern(sql = r#"
@@ -379,29 +290,31 @@ fn jsonb_test(datum: pg_sys::Datum) -> bool {
     AS '@MODULE_PATHNAME@', '@FUNCTION_NAME@';
 "#)]
 fn jsonb_to_text(datum: pg_sys::Datum) -> Option<String> {
-    let varlena = datum.cast_mut_ptr();
-    let detoasted = unsafe { pg_sys::pg_detoast_datum_packed(varlena) };
-
-    let result = unsafe {
-        iterate_jsonb(
-            detoasted as *mut pg_sys::Jsonb,
-            SerdeValueBuilder::default(),
-        )
-        .unwrap()
-    };
-
-    if detoasted != varlena {
-        unsafe {
-            pg_sys::pfree(detoasted as pgrx::void_mut_ptr);
-        }
-    }
+    let blob = JsonbBlob::new(datum.cast_mut_ptr());
+    let mut it = JsonbIterator::new(blob);
+    let value = read_value(&mut it);
 
     // Ensure nothing gets optimized away
-    Some(serde_json::to_string(&result).expect("should be able to print serde_json::Value"))
+    Some(serde_json::to_string(&value).expect("should be able to print serde_json::Value"))
 }
 
 #[pg_extern]
 fn jsonb_test2(datum: pgrx::JsonB) -> bool {
     // Ensure nothing gets optimized away
     matches!(datum.0, serde_json::Value::Object(_))
+}
+
+#[pg_extern(sql = r#"
+    CREATE FUNCTION "jsonb_test"(jsonb) RETURNS boolean
+    STRICT
+    LANGUAGE c /* Rust */
+    AS '@MODULE_PATHNAME@', '@FUNCTION_NAME@';
+"#)]
+fn jsonb_test3(datum: pg_sys::Datum) -> bool {
+    let blob = JsonbBlob::new(datum.cast_mut_ptr());
+    let mut it = JsonbIterator::new(blob);
+    let value = read_value(&mut it);
+
+    // Ensure nothing gets optimized away
+    matches!(value, serde_json::Value::Object(_))
 }
